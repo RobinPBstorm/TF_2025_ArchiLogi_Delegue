6f8cfed47f9721b513d1c4ed0e87d4bbd4ae1158
robinPecheurBstorm <robin.pecheur@bstorm.be>
2025-10-16 12:38:15 +0200

    Demo sur les événements

---
+++ TF_2025_ArchiLogi_Delegue/ModelsPourEvenement/BeAlerte.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TF_2025_ArchiLogi_Delegue.ModelsPourEvenement
+{
+    public delegate void SurAlerteRecue(string message);
+    public class BeAlerte
+    {
+        // event => il n'y a que la classe qui l'a créé
+        // qui pourra l'invoquer
+        public event SurAlerteRecue del = null;
+
+        public void Ajouter(SurAlerteRecue methodePourContacter)
+        {
+            // ajoute la méthode dans "la liste" des fonctions
+            del += methodePourContacter;
+        }
+        public void Retirer(SurAlerteRecue methodePourContacter)
+        {
+            // retire la méthode dans "la liste" des fonctions
+            del -= methodePourContacter;
+        }
+
+        public void EnvoyerAlerte(string message)
+        {
+            if (del is not null)
+            {
+                // l'invocation de toutes méthodes stocker dans le délégué
+                del(message);
+
+                foreach (Delegate method in del.GetInvocationList())
+                {
+                    try
+                    {
+                        ((SurAlerteRecue)method)(message);
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
--- TF_2025_ArchiLogi_Delegue/Program.cs
+++ TF_2025_ArchiLogi_Delegue/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using AutreLangage = TF_2025_ArchiLogi_Delegue.Models;
 using CSharp = TF_2025_ArchiLogi_Delegue.ModelsPourVraiDelegue;
+using CSharpEvent = TF_2025_ArchiLogi_Delegue.ModelsPourEvenement;
 
 Console.WriteLine("Observable classique");
 #region Observable dans les autres langages
@@ -27,6 +28,11 @@ CSharp.Citoyen citoyen4 = new CSharp.Citoyen("Laurie");
 beAlerte2.Ajouter(citoyen3.RecevoirEmail);
 beAlerte2.Ajouter(citoyen3.ContaterParPigeon);
 //beAlerte2.Retirer(citoyen3.ContaterParPigeon);
+beAlerte2.Ajouter(
+    delegate (string message)
+    {
+        Console.WriteLine(message);
+    });
 
 beAlerte2.Ajouter(citoyen4.RecevoirEmail);
 // /!\ un délégué stocke l'espace mémoire où se trouve
@@ -41,5 +47,31 @@ beAlerte2.EnvoyerAlerte("Pénurie de pattate");
 #endregion
 
 
+Console.WriteLine();
+Console.WriteLine("Utilisation de Evenement");
+#region Délégués en C#
+CSharpEvent.BeAlerte beAlerteEvent = new CSharpEvent.BeAlerte();
+
+CSharpEvent.Citoyen citoyenEvent1 = new CSharpEvent.Citoyen("John");
+CSharpEvent.Citoyen citoyenEvent2 = new CSharpEvent.Citoyen("Gérard");
+
+// On ajoute la méthode qui sera stocké dans le délégué
+beAlerteEvent.Ajouter(citoyenEvent1.RecevoirEmail);
+beAlerteEvent.Ajouter(citoyenEvent1.ContaterParPigeon);
+//beAlerteEvent.Retirer(citoyenEvent1.ContaterParPigeon);
+
+beAlerteEvent.Ajouter(citoyenEvent2.RecevoirEmail);
+// /!\ un délégué stocke l'espace mémoire où se trouve
+// la méthode à employer
+// => le délgué ignorera l'accessibilité de cette méthode
+citoyenEvent2.AjouterNoteAlertePriveeABeAlerte(beAlerteEvent);
+
+// Envoi un message via toutes les méthodes stockées
+beAlerteEvent.EnvoyerAlerte("Pénurie de pattate");
+
+// Impossible ici car c'est un événement
+//beAlerteEvent.del("Message d'un pirate");
+
+#endregion
 
 
